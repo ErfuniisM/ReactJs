8484034a97d90ee987e4f948417f5c497f45c658
Erfan <erfuniism@gmail.com>
2023-02-26 08:59:28 +0000

    Add Image in WebSite

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,12 @@ import Lightyear from "./Images/Lightyear.gif";
 import "./App.css";
 
 function App() {
-  return <h1>Hello World!</h1>;
+  return (
+    <div>
+      <h1>Hello World!</h1>
+      <img src={Lightyear} alt="" />
+    </div>
+  );
 }
 
 export default App;
